ec7605f9a41c69dca3f8b7712125c7643241189e
Tobi-De <tobidegnon@protonmail.com>
2021-10-22 22:29:58 +0100

    remove cors origin

--- app/core/config.py
+++ app/core/config.py
@@ -1,20 +1,20 @@
-from typing import List, Optional, Union
+from typing import Optional
 
-from pydantic import AnyHttpUrl, BaseSettings, validator
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: Optional[str] = "projects-api"
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    # BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
     DETA_PROJECT_KEY: str
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
+    # @validator("BACKEND_CORS_ORIGINS", pre=True)
+    # def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+    #     if isinstance(v, str) and not v.startswith("["):
+    #         return [i.strip() for i in v.split(",")]
+    #     elif isinstance(v, (list, str)):
+    #         return v
+    #     raise ValueError(v)
 
     class Config:
         case_sensitive = True
