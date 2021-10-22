0285b618603fd899a017913788c1ec80618bdb95
Tobi-De <tobidegnon@protonmail.com>
2021-10-22 13:29:13 +0100

    rename app to micro

--- micro/main.py
+++ micro/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.core.config import settings
+from .core.config import settings
 from .endpoints import router
 
 
---
+++ runserver.sh
@@ -0,0 +1,1 @@
+uvicorn micro.main:app --reload
